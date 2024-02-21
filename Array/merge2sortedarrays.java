68ac5de5a7a8ad32242b8ac4e5ea0c6e9f41597a
Rajat Verma <96742104+Rajat-eng@users.noreply.github.com>
2024-02-21 11:17:20 +0530

    Update merge2sortedarrays.java

--- Array/merge2sortedarrays.java
+++ Array/merge2sortedarrays.java
@@ -1,41 +1,49 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-public class merge2sortedarrays {
-	public static int[] merge(int []arr1, int []arr2) {
-		int i=0,j=0;
-        int n=arr1.length;
-        int m=arr2.length;
-        int k=0;
-        int arr[]=new int[n+m];
-            while(i<n && j<m){
-            if(arr1[i]>arr2[j]){ 
-                arr[k]=arr2[j];
-                j++; 
-                k++;
+class HelloWorld {
+    public static int[] sort(int []arr,int left,int right){
+        if(left==right){
+            int ele=arr[left];
+            return new int[]{ele};
+        }
+        int m=left +(right-left)/2;
+        int [] LeftArray =sort(arr,left,m);
+        int [] RightArray=sort(arr,m+1,right);
+        return merge(LeftArray, RightArray);
+    } 
+    
+    private static int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                merged[k++] = left[i++];
+            } else {
+                merged[k++] = right[j++];
             }
-            else{
-                arr[k]=arr1[i];
-                i++;
-                k++;
-            }          
-          }
-            while(i<n) {
-            	arr[k]=arr1[i];
-            	k++;
-            	i++;
-            }
-            while(j<m){
-            	arr[k]=arr2[j];
-            	k++;
-            	j++;
-            }      
-       return arr; 
-   }
-	public static void main(String[] args) {
-		int arr1[]= {1,3,5,7};
-		int arr2[]= {2,4,6};
-		int[] arr= merge(arr1,arr2);
-		for(int k=0;k<arr.length;k++) {
-			System.out.print(arr[k]+" ");
-		}
-   }
+        }
+        
+        while (i < left.length) {
+            merged[k++] = left[i++];
+        }
+        
+        while (j < right.length) {
+            merged[k++] = right[j++];
+        }
+        
+        return merged;
+    }
+      public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+       int []arr=new int[]{3,2,1};
+       int []ans=sort(arr,0,arr.length-1);
+       printArray(ans);
+    }
 }
