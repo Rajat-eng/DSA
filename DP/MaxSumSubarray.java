821b5a61b6d3143c245b94497477771078b99449
Rajat Verma <96742104+Rajat-eng@users.noreply.github.com>
2024-03-23 17:21:04 +0530

    Update MaxSumSubarray.java

--- DP/MaxSumSubarray.java
+++ DP/MaxSumSubarray.java
@@ -2,23 +2,33 @@ package DP;
 
 public class MaxSumSubarray {
 
-	private static int solve(int[] arr) {
-		int csum=arr[0];
-		int osum=arr[0];
-		for(int i=1;i<arr.length;i++) {
-			if(csum>0) {
-				csum+=arr[i];
-			}else {
-				csum=arr[i];
+	public static long maxSubarraySum(int[] arr, int n) {
+
+		if(n==0){
+			return 0L;
+		}
+		
+		long totalSum=0;
+		long tempSum=0;
+		for(int i=0;i<n;i++){
+			
+			tempSum+=arr[i];
+			
+			if(tempSum>=totalSum){
+				totalSum=tempSum;
 			}
-			if(csum>osum) {
-				osum=csum;
+
+			if(tempSum<=0){
+				tempSum=0;
 			}
 		}
-		return osum;
+		if(totalSum<=0){
+			return 0L;
+		}
+		return totalSum;
 	}
 	public static void main(String[] args) {
 		int [] arr= {4,3,-2,6,7,-10,-10,4,5,9,-3,4,7,-28,2,9,3,2,11};
-		System.out.println(solve(arr));
+		System.out.println(maxSubarraySum(arr,arr.length));
 	}
 }
